a58df496434ac8344b8ccbd38632385a5066087e
Krakov <93730442+KrakovBen@users.noreply.github.com>
2025-07-31 18:55:15 +0300

    Style

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
-import Container from "@mui/material/Container"
-import Typography from "@mui/material/Typography"
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import ROUTES from './../routes/routesModel'
 
 const ErrorPage = () => {
     const navigate = useNavigate()
     return (
         <Container>
-            <Typography variant="h1">ErrorPage</Typography>
+            <Typography variant='h1'>ErrorPage</Typography>
 
             <Grid container spacing={2}>
                 <Grid item xs={12} md={8}>
-                <Typography variant="h5" color="initial">
+                <Typography variant='h5' color='initial'>
                     Oops... The requested URL was not found on this server
                 </Typography>
-                <Button variant="text" color="primary" onClick={()=>navigate(ROUTES.CARDS)}>
+                <Button variant='text' color='primary' onClick={()=>navigate(ROUTES.CARDS)}>
                     Click here to return to the home page...
                 </Button>
                 </Grid>
-                <Grid item xs={12} md={4} justifyContent="center">
+                <Grid item xs={12} md={4} justifyContent='center'>
                 <img
-                    width="100%"
-                    src="/assets/images/broken-robot.png"
-                    alt="broken robot"
+                    width='100%'
+                    src='/assets/images/broken-robot.png'
+                    alt='broken robot'
                 />
                 </Grid>
             </Grid>
