6d4a42e62a07833603afa2807a6bd48caf1e124a
Krakov <93730442+KrakovBen@users.noreply.github.com>
2025-08-01 12:14:24 +0300

    User login page style

--- client/src/forms/components/Input.jsx
+++ client/src/forms/components/Input.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { string, bool, object, func } from 'prop-types'
-import TextField from '@mui/material/TextField'
-import { makeFirstLetterCapital } from '../../utils/algoMethods'
 import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import InputBase from '@mui/material/InputBase'
 
 const Input = ({ variant, type, name, data, label, required, error, onChange, ...rest }) => {
     return (
         <Grid sx={{ gridColumn: 'span 12', mt: 2 }} {...rest}>
-            <TextField variant={variant} label={makeFirstLetterCapital(label)} type={type} id={name} name={name} value={data[name] ? data[name] : ''} required={required} helperText={error} error={Boolean(error)} onChange={onChange} fullWidth autoComplete='off' dir="rtl" />
+            {/* <TextField variant={variant} label={makeFirstLetterCapital(label)} type={type} id={name} name={name} value={data[name] ? data[name] : ''} required={required} helperText={error} error={Boolean(error)} onChange={onChange} fullWidth autoComplete='off' dir="rtl" /> */}
+            <Typography variant='h6' component='p'>{required ? `${label} *` : label}</Typography>
+            <InputBase id={name} name={name} type={type} value={data[name]} onChange={onChange} required={required} fullWidth sx={{ borderBottom: '1px solid', borderColor: error ? 'error.main' : 'grey.400', '&:focus-within': { borderColor: 'primary.main' } }}/>
+
+            {error && (
+                <Typography variant="caption" color="error">
+                    {error}
+                </Typography>
+            )}
         </Grid>
     )
 }
